f57080cd677bde39917d23f071b86ec1c8a5c914
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-09-13 10:41:05 +0100

    Initial commit with initial C++ code

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment
+    /*
+        And this is a 
+        multi-line comment
+    */
+
+    std::cout << "I like natto!" << '\n';
+    std::cout << "It's kinda sticky and gross, but I like it!" << '\n';
+    return 0;
+}
